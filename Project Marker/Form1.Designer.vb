739adf10624b7e6e438ede016eafc14aea6380f3
Malang Baram <malangbalam@darklambs.net>
2015-08-12 23:30:40 +0900

    첫 커밋 by 호박고구마

---
+++ Project Marker/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(348, 32)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(178, 69)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "다운로드 시작"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(15, 9)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(863, 17)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(393, 57)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.Text = "croatiawiki.dothome.co.kr/Mods.7z"
+        Me.TextBox1.Visible = False
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(12, 12)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.ScrollBarsEnabled = False
+        Me.WebBrowser1.Size = New System.Drawing.Size(863, 336)
+        Me.WebBrowser1.TabIndex = 5
+        Me.WebBrowser1.Url = New System.Uri("http://puyoin.me/TeamCroatia/page/main/", System.UriKind.Absolute)
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Panel1.Controls.Add(Me.ProgressBar1)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Location = New System.Drawing.Point(-3, 376)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(896, 124)
+        Me.Panel1.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BackgroundImage = Global.Project_Marker.My.Resources.Resources.quartz_ore
+        Me.ClientSize = New System.Drawing.Size(887, 480)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(903, 519)
+        Me.MinimumSize = New System.Drawing.Size(903, 519)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Croatia Client  Beta 1.9"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ Project Marker/Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Net
+
+Public Class Form1
+    Public 경로 = Environ("appdata") & "\.minecraft\Mods.7z"
+    Public WithEvents download As WebClient
+    Dim Myprocess1 As System.Diagnostics.Process
+    Dim Process1 As New ProcessStartInfo
+
+    Private Sub download_DownloadProgressChanged(ByVal sender As Object, ByVal e As System.Net.DownloadProgressChangedEventArgs) Handles download.DownloadProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        download = New WebClient
+        download.DownloadFileTaskAsync("https://www.dropbox.com/s/j7uwpxin3eygkop/Mods.7z?dl=1", 경로)
+        My.Computer.FileSystem.WriteAllBytes(Environ("appdata") & "\.minecraft\7z.exe", My.Resources._7z, True)
+        Process1.FileName = Environ("appdata") & "\.minecraft\7za.exe"
+        Process1.Arguments = "x -y " & """" & Environ("appdata") & "\.minecraft\" & "Mods.7z""" & " -o" & """" & Environ("appdata") & "\.minecraft\"""
+        Process1.WindowStyle = ProcessWindowStyle.Hidden
+        Myprocess1 = Process.Start(Process1)
+        Myprocess1.WaitForExit()
+        MsgBox("완료되었습니다")
+    End Sub
+
+End Class
